0fc7fb030c3e57895eed2497f9db441b4911d7e2
fatih <fatihkarakus@hotmail.com>
2020-07-07 19:07:23 +0200

    Updated add-number.js

--- scripts/user-actions/add-number.js
+++ scripts/user-actions/add-number.js
@@ -1,26 +1,26 @@
-'use strict';
-console.log('--- loading: add number');
-
+"use strict";
+console.log("--- loading: add number");
+debugger;
 const addNumberHandler = () => {
-  console.log('\n--- calling: add number handler');
+  console.log("\n--- calling: add number handler");
 
-  const userInput = _;
-  console.log('userInput:', typeof userInput, '\n', userInput);
-  if (_) {
+  const userInput = prompt("enter a number to add:");
+  console.log("userInput:", typeof userInput, "\n", userInput);
+  if (userInput === null || userInput === "") {
     return;
   }
 
-  const userNumber = _;
-  console.log('userNumber:', typeof userNumber, '\n', userNumber);
-  if (_) {
+  const userNumber = Number(userInput);
+  console.log("userNumber:", typeof userNumber, "\n", userNumber);
+  if (isNaN(userNumber)) {
     alert(`${userInput} is not a valid number`);
     return;
   }
 
-  console.log('numbers (before):', typeof numbers, '\n', numbers);
+  console.log("numbers (before):", typeof numbers, "\n", numbers);
   // add the user's number to the main shared array of numbers
-  _;
-  console.log('numbers (after):', typeof numbers, '\n', numbers);
+  numbers.push(userNumber);
+  console.log("numbers (after):", typeof numbers, "\n", numbers);
 
   displayArrayHandler();
 };
